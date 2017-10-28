487fdcdde99a8a5d68fa5b1c50d9b2819daf5e62
acongos92 <30706447+acongos92@users.noreply.github.com>
2017-10-28 14:36:10 -0400

    Initial commit

---
+++ app/src/main/java/com/android/customsms/MessagingMainScreen.java
@@ -0,0 +1,13 @@
+package com.android.customsms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessagingMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging_main_screen);
+    }
+}
