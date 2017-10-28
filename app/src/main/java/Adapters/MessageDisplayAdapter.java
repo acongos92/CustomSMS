17384025b73481186cae869317d28e233a3fa140
acongos92 <30706447+acongos92@users.noreply.github.com>
2017-10-28 15:09:47 -0400

    not done but available for work

---
+++ app/src/main/java/Adapters/MessageDisplayAdapter.java
@@ -0,0 +1,27 @@
+package Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.customsms.R;
+
+/**
+ * Created by zigza on 10/28/2017.
+ */
+
+public class MessageDisplayAdapter extends RecyclerView.Adapter<MessageDisplayAdapter.MessageDisplayAdapterViewHolder> {
+
+
+
+    class MessageDisplayAdapterViewHolder extends RecyclerView.ViewHolder {
+        protected TextView fromView;
+        protected TextView bodyView;
+        public MessageDisplayAdapterViewHolder(View itemView){
+            super(itemView);
+            fromView = (TextView) itemView.findViewById(R.id.tv_from_sender);
+            bodyView = (TextView) itemView.findViewById(R.id.tv_body_preview);
+
+        }
+    }
+}
--- app/src/main/java/com/android/customsms/MessagingMainScreen.java
+++ app/src/main/java/com/android/customsms/MessagingMainScreen.java
@@ -1,13 +1,83 @@
 package com.android.customsms;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MessagingMainScreen extends AppCompatActivity {
-
+    ArrayList<String> messageList = new ArrayList<String>();
+    RecyclerView recyclerView;
+    MessageDisplayAdapter messageDisplayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging_main_screen);
+    }
+
+
+    /*
+     * permission check and requests
+     */
+
+    public void getPermissionToReadSMS(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED){
+            if(shouldShowRequestPermissionRationale(Manifest.permission.READ_SMS)){
+                makeToast("Please allow permission");
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_SMS}, 1);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == 1) {
+            if (grantResults.length == 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read SMS permission granted", Toast.LENGTH_SHORT).show();
+                //refreshSmsInbox();
+            } else {
+                Toast.makeText(this, "Read SMS permission denied", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    //Convenience methods
+
+    private void makeToast(String message){
+        Context context = getApplicationContext();
+        CharSequence text = message;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void refreshSmsInbox() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        int indexBody = smsInboxCursor.getColumnIndex("body");
+        int indexAddress = smsInboxCursor.getColumnIndex("address");
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
+        arrayAdapter.clear();
+        do {
+            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +
+                    "\n" + smsInboxCursor.getString(indexBody) + "\n";
+            arrayAdapter.add(str);
+        } while (smsInboxCursor.moveToNext());
     }
 }
